f1d9ccbbebbfd935a7691b56ffbe37d178acf52b
Bahoz <bahozali50@gmail.com>
2022-07-04 17:30:41 +0300

    setup the library project

--- README.md
+++ README.md
@@ -1,56 +1,35 @@
 ![](https://img.shields.io/badge/Microverse-blueviolet)
 
-# Project Name
+# Simple Library
 
-> Description the project.
-
+>  This is simple project about adding, removing new book, a building or room containing collections of books, periodicals, and   for use or borrowing by the public or the members of an institution. 
 
 ## Built With
 
-- Major languages
-- Frameworks
-- Technologies used
+- HTML
+- CSS
+- JavaScript
+- Git and github
 
-## Live Demo (if available)
+## Live Demo
 
-[Live Demo Link](https://livedemo.com)
-
+[Live Demo Link](https://bahoz-ali.github.io/simple_library_v2/)
 
 ## Getting Started
 
-**This is an example of how you may give instructions on setting up your project locally.**
-**Modify this file to match your project, remove sections that don't apply. For example: delete the testing section if the currect project doesn't require testing.**
-
-
-To get a local copy up and running follow these simple example steps.
-
-### Prerequisites
-
-### Setup
-
-### Install
-
-### Usage
-
-### Run tests
-
-### Deployment
-
-
+- `git clone git@github.com:bahoz-ali/simple-library.git`
 
 ## Authors
 
-👤 **Author1**
+👤 **Bahoz Ali**
 
-- GitHub: [@githubhandle](https://github.com/githubhandle)
-- Twitter: [@twitterhandle](https://twitter.com/twitterhandle)
-- LinkedIn: [LinkedIn](https://linkedin.com/in/linkedinhandle)
+- GitHub: [@githubhandle](https://github.com/bahoz-ali)
+- LinkedIn: [LinkedIn](https://linkedin.com/in/bahoz-ali)
 
-👤 **Author2**
+👤 **Chimwemwe Mkandawire**
 
-- GitHub: [@githubhandle](https://github.com/githubhandle)
-- Twitter: [@twitterhandle](https://twitter.com/twitterhandle)
-- LinkedIn: [LinkedIn](https://linkedin.com/in/linkedinhandle)
+- GitHub: [@githubhandle](https://github.com/chimwemwe007)
+- Twitter: [@twitterhandle](https://twitter.com/CHxMZMK)
 
 ## 🤝 Contributing
 
@@ -71,3 +50,4 @@ Give a ⭐️ if you like this project!
 ## 📝 License
 
 This project is [MIT](./MIT.md) licensed.
+
---
+++ scripts/index.js
@@ -0,0 +1,151 @@
+/* eslint-disable max-classes-per-file */
+const titleInput = document.querySelector('#title_input');
+const authorInput = document.querySelector('#author_input');
+const bookList = document.querySelector('.book_list');
+const addBookButton = document.querySelector('#add_book');
+const form = document.querySelector('.book_section form');
+
+// sections
+const showingBooksSection = document.querySelector('.showing_books');
+const newBookSection = document.querySelector('.book_section');
+const contactSection = document.querySelector('.contact_section');
+
+// navigation links
+const homeLink = document.querySelector('#home_link');
+const addBookLink = document.querySelector('#add_book_link');
+const contactLink = document.querySelector('#contact_link');
+
+class Book {
+  constructor(id, title, authorName) {
+    this.id = id;
+    this.title = title;
+    this.authorName = authorName;
+  }
+}
+
+class Library {
+  books = [];
+
+  uniqueId = () => {
+    const { length } = this.books;
+    return length ? length + 1 : 0;
+  };
+
+  addBook = (e) => {
+    e.preventDefault();
+
+    const id = this.uniqueId();
+    const title = titleInput.value.trim() ? titleInput.value.trim() : 'test';
+    const author = authorInput.value.trim() ? authorInput.value.trim() : 'test';
+
+    const newBook = new Book(id, title, author);
+
+    this.books.push(newBook);
+
+    this.updateStorage();
+
+    this.displayBooks();
+
+    form.reset();
+  };
+
+  // eslint-disable-next-line
+  deleteBook = (id) => {
+    if (this.books) {
+      this.books = this.books.filter((b) => b.id !== id);
+
+      this.updateStorage();
+      this.displayBooks();
+    }
+  };
+
+  getBooks = () => {
+    try {
+      return JSON.parse(localStorage.getItem('books'));
+    } catch (error) {
+      return localStorage.getItem('books');
+    }
+  };
+
+  createLocalStorage() {
+    if (!localStorage.getItem('books')) {
+      localStorage.setItem('books', JSON.stringify([]));
+    } else {
+      this.books = this.getBooks();
+    }
+  }
+
+  updateStorage() {
+    localStorage.setItem('books', JSON.stringify(this.books));
+  }
+
+  templateBook = (obj) => {
+    const div = document.createElement('div');
+    div.classList.add('book');
+
+    const insideBook = `
+     <p><i class="fa-solid fa-address-book"></i> <span style="margin-left: 0.2rem;">${obj.title}</span> - <span>${obj.authorName}</span></p>
+             <button class="btn" id="delete_book" type="submit" onClick="library.deleteBook(${obj.id})" data-id='${obj.id}'><i class="fa-solid fa-trash"></i></button>`;
+
+    div.innerHTML = insideBook;
+
+    bookList.append(div);
+  };
+
+  displayBooks() {
+    // clean the book list before.
+    bookList.innerHTML = '';
+
+    if(this.books.length === 0) {
+      hide(showingBooksSection)
+    }
+
+    if (this.books || this.books.length !== 0) {
+      this.books.forEach((book) => {
+        this.templateBook(book);
+      });
+    }
+  }
+}
+
+function show(name, display = 'block') {
+  name.style.display = display;
+}
+
+function hide(name) {
+  name.style.display = 'none';
+}
+
+const library = new Library();
+
+homeLink.addEventListener('click', () => {
+  if (library.books.length) {
+    show(showingBooksSection);
+  }
+
+  hide(newBookSection);
+  hide(contactSection);
+  library.displayBooks();
+});
+
+addBookLink.addEventListener('click', () => {
+  show(newBookSection, 'flex');
+  hide(showingBooksSection);
+  hide(contactSection);
+});
+
+contactLink.addEventListener('click', () => {
+  show(contactSection, 'flex');
+  hide(showingBooksSection);
+  hide(newBookSection);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  library.createLocalStorage();
+  library.displayBooks();
+  addBookButton.addEventListener('click', library.addBook);
+
+  if (library.books.length) {
+    show(showingBooksSection);
+  }
+});
